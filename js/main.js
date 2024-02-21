05c85cc6bd0cf0aecc8fa6097786f519b8b969ca
Iscander <117932482+zIscander@users.noreply.github.com>
2024-02-21 13:13:41 +0500

    Изменил фон, добавил паралакс на активный фон

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// Функция нажатия на стрелку 
 function scrollDown() {
   window.scrollBy(0, 300); // Прокрутка сайта на 200px вниз
 }
@@ -5,10 +6,19 @@ function scrollUp() {
   window.scrollBy(0, -300); // Прокрутка сайта на 200px вверх
 }
 
-// Скролл к блоку main
+// Скролл к блоку main по нажатию на "Начало"
 function scrollToMain() {
   window.scrollTo({
     top: 300,
     behavior: 'smooth'
   });
 }
+
+// Паралакс эффект для активного фона
+const bgActive = document.querySelector('.bg-active');
+
+document.addEventListener('mousemove', function(e) {
+  const posX = e.clientX / window.innerWidth / 1.5;
+  const posY = e.clientY / window.innerHeight / 1.5;
+  bgActive.style.translate = `${posX}% ${posY}%`;
+});
